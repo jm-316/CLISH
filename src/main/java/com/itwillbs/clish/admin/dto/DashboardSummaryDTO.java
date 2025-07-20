fa6d67e01d64d43691928c85bad7ee8dce7864e9
hopp3 <hopp3@DESKTOP-S93VRNA>
2025-07-20 23:58:30 +0900

    admin/ dashboard 관련 DTO 추가

---
+++ src/main/java/com/itwillbs/clish/admin/dto/DashboardSummaryDTO.java
@@ -0,0 +1,15 @@
+package com.itwillbs.clish.admin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DashboardSummaryDTO {
+	private int userCount;
+	private int companyCount;
+	private int pendingClassCount;
+	private int pendingCompanyCount;
+}
